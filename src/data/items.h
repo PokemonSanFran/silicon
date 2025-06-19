b6d7f28e1155e94c5850cd72eb2188abcc3b4fc4
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-06-19 14:34:09 -0700

    feat (wildEncounters): Added new constant for Fishing Rod and updated Super Rod name and description to match the Fishing Rod

--- src/data/items.h
+++ src/data/items.h
@@ -12489,12 +12489,16 @@ const struct Item gItemsInfo[] =
 
     [ITEM_SUPER_ROD] =
     {
-        .name = _("Super Rod"),
+        // Start wildEncounters
+        //.name = _("Super Rod"),
+        .name = _("Fishing Rod"),
         .price = 0,
         .description = COMPOUND_STRING(
-                "The best fishing\n"
+                //"The best fishing\n"
+                "A fishing\n"
                 "rod for catching\n"
                 "wild Pokémon."),
+        // End wildEncounters
         .importance = 1,
         .pocket = POCKET_KEY_ITEMS,
         .type = ITEM_USE_FIELD,
