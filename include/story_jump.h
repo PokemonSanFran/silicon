1afff7a8831bf0d24a10a1de0bfa3f2865522edd
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-08-02 13:09:01 -0700

    feat (trainerBattles): Made cleargymtrainersandgetbadge macro easier to use and added to all gyms

--- include/story_jump.h
+++ include/story_jump.h
@@ -8,5 +8,6 @@ extern u8 ThereCanOnlyBeOne_Debug_Script[];
 
 void JumpPlayerToStoryPoint(u32 chosenStoryPoint, bool32 jumpType);
 void WarpPlayerAfterVarSet(void);
+void ClearGymTrainer_Script(void);
 
 #endif // GUARD_STORYJUMP_H
