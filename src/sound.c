5ecf781a27839015a1baded8885ece7b3c1a772e
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-10-25 11:37:36 -0700

    feat (firstMusicUpdate): Added new badge jingle to the fanfare table

--- src/sound.c
+++ src/sound.c
@@ -54,6 +54,7 @@ static const struct Fanfare sFanfares[] = {
     [FANFARE_OBTAIN_B_POINTS]     = { MUS_OBTAIN_B_POINTS,     313 },
     [FANFARE_OBTAIN_SYMBOL]       = { MUS_OBTAIN_SYMBOL,       318 },
     [FANFARE_REGISTER_MATCH_CALL] = { MUS_REGISTER_MATCH_CALL, 135 },
+    [FANFARE_GET_BADGE]           = { MUS_GET_BADGE,           420 },
 };
 
 void InitMapMusic(void)
