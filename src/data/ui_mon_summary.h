ca8565e8db27a296f36d8efd13e6d5c021f3d42c
mudskipper13 <MariaJumpMan001@hotmail.com>
2026-01-07 09:12:43 +0700

    feat (monSummary): move const data onto src/data/

---
+++ src/data/ui_mon_summary.h
@@ -0,0 +1,233 @@
+static const struct BgTemplate sSummarySetup_BgTemplates[NUM_SUMMARY_BACKGROUNDS] =
+{
+    {
+        .bg = SUMMARY_BG_TEXT,
+        .charBaseIndex = 0,
+        .mapBaseIndex = 31,
+        .priority = 0
+    },
+    {
+        .bg = SUMMARY_BG_PAGE_1,
+        .charBaseIndex = 2,
+        .mapBaseIndex = 28,
+        .screenSize = 1,
+        .priority = 1
+    },
+    {
+        .bg = SUMMARY_BG_PAGE_2,
+        .charBaseIndex = 2,
+        .mapBaseIndex = 26,
+        .screenSize = 1,
+        .priority = 1
+    },
+};
+
+static const struct WindowTemplate sSummarySetup_MainWindows[NUM_SUMMARY_MAIN_WINDOWS + 1] =
+{
+    [SUMMARY_MAIN_WIN_HEADER] =
+    {
+        .bg = SUMMARY_BG_TEXT,
+        .tilemapLeft = 23,
+        .tilemapTop = 0,
+        .width = 4,
+        .height = 4,
+    },
+
+    [SUMMARY_MAIN_WIN_TEXT_BOX] =
+    {
+        .bg = SUMMARY_BG_TEXT,
+        .tilemapLeft = 0,
+        .tilemapTop = 14,
+        .width = DISPLAY_TILE_WIDTH,
+        .height = 4
+    },
+
+    [SUMMARY_MAIN_WIN_HELP_BAR] =
+    {
+        .bg = SUMMARY_BG_TEXT,
+        .tilemapLeft = 0,
+        .tilemapTop = 18,
+        .width = DISPLAY_TILE_WIDTH,
+        .height = 2,
+    },
+
+    // SUMMARY_MAIN_WIN_DYNAMIC uses AddWindow
+
+    DUMMY_WIN_TEMPLATE
+};
+
+static const struct MonSummarySprite sSummarySetup_MainSprites[NUM_SUMMARY_MAIN_SPRITES] =
+{
+    {
+        .id = SUMMARY_MAIN_SPRITE_SHINY_SYMBOL,
+        .oam = &(const struct OamData){
+            .shape = SPRITE_SHAPE(16x16), .size = SPRITE_SIZE(16x16),
+            .priority = 0
+        },
+        .tileTag = TAG_SUMMARY_SHINY_SYMBOL,
+        .gfx = (const u32[])INCBIN_U32("graphics/ui_menus/mon_summary/shiny_symbol.4bpp.smol"),
+        .anims = gDummySpriteAnimTable,
+        .callback = SpriteCB_SummarySprite_ShinySymbol
+    },
+    {
+        .id = SUMMARY_MAIN_SPRITE_HP_BAR,
+        .oam = &(const struct OamData){
+            .shape = SPRITE_SHAPE(64x32), .size = SPRITE_SIZE(64x32),
+            .priority = 0
+        },
+        .tileTag = TAG_SUMMARY_HP_BAR,
+        .gfx = gBlankGfxCompressed,
+        .anims = gDummySpriteAnimTable,
+        .callback = SpriteCB_SummarySprite_HpBar
+    },
+    {
+        .id = SUMMARY_MAIN_SPRITE_EXP_BAR,
+        .subsprites = (const struct SubspriteTable[]){
+            {
+                .subspriteCount = 2,
+                .subsprites = (const struct Subsprite[]){
+                    {
+                        .x = 0, .y = 0,
+                        .shape = SPRITE_SHAPE(32x8), .size = SPRITE_SIZE(32x8),
+                        .tileOffset = 0
+                    },
+                    {
+                        .x = 32, .y = 0,
+                        .shape = SPRITE_SHAPE(32x8), .size = SPRITE_SIZE(32x8),
+                        .tileOffset = 4
+                    },
+                },
+            },
+            { 0 },
+        },
+        .oam = &(const struct OamData){
+            .shape = SPRITE_SHAPE(8x8), .size = SPRITE_SIZE(8x8),
+            .priority = 0
+        },
+        .tileTag = TAG_SUMMARY_EXP_BAR,
+        .gfx = gBlankGfxCompressed,
+        .anims = gDummySpriteAnimTable,
+        .callback = SpriteCB_SummarySprite_ExpBar
+    },
+    {
+        .id = SUMMARY_MAIN_SPRITE_FRIENDSHIP_BAR,
+        .subsprites = (const struct SubspriteTable[]){
+            {
+                .subspriteCount = 2,
+                .subsprites = (const struct Subsprite[]){
+                    {
+                        .x = 0, .y = 0,
+                        .shape = SPRITE_SHAPE(32x8), .size = SPRITE_SIZE(32x8),
+                        .tileOffset = 0
+                    },
+                    {
+                        .x = 32, .y = 0,
+                        .shape = SPRITE_SHAPE(8x8), .size = SPRITE_SIZE(8x8),
+                        .tileOffset = 4
+                    },
+                },
+            },
+            { 0 },
+        },
+        .oam = &(const struct OamData){
+            .shape = SPRITE_SHAPE(8x8), .size = SPRITE_SIZE(64x32),
+            .priority = 0
+        },
+        .tileTag = TAG_SUMMARY_FRIENDSHIP_BAR,
+        .gfx = gBlankGfxCompressed,
+        .anims = gDummySpriteAnimTable,
+        .callback = SpriteCB_SummarySprite_FriendshipBar
+    },
+};
+
+static const u8 sSummarySprite_HpBarBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/hp_bar.4bpp");
+static const u16 sSummarySprite_HpBarColors[] = INCBIN_U16("graphics/ui_menus/mon_summary/hp_bar_states.gbapal");
+static const u8 sSummarySprite_ExpBarBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/exp_bar.4bpp");
+static const u8 sSummarySprite_FriendshipBarBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/friendship_bar.4bpp");
+
+static const TaskFunc sSummaryMode_InputFuncs[NUM_SUMMARY_MODES] =
+{
+    [SUMMARY_MODE_DEFAULT] = Task_SummaryMode_DefaultInput,
+};
+
+static const struct MonSummaryPageInfo sSummaryPage_Info[NUM_SUMMARY_PAGES] =
+{
+    [SUMMARY_PAGE_INFOS] =
+    {
+        .name = COMPOUND_STRING("Info"),
+        .windows =
+        {
+            {
+                .id = SUMMARY_INFOS_WIN_HEADER,
+                .left = 8, .top = 0,
+                .width = 9, .height = 4
+            },
+            {
+                .id = SUMMARY_INFOS_WIN_SUMMARY,
+                .left = 15, .top = 4,
+                .width = 15, .height = 10
+            },
+            {
+                .id = SUMMARY_INFOS_WIN_MISC,
+                .left = 0, .top = 9,
+                .width = 13, .height = 5
+            },
+            SUMMARY_DYNAMIC_WIN_DUMMY
+        },
+        .tilemap = (const u32[])INCBIN_U32("graphics/ui_menus/mon_summary/pages/infos.bin.smolTM"),
+        .mainSpriteCoords =
+        {
+            [SUMMARY_MAIN_SPRITE_SHINY_SYMBOL] = { SUMMARY_INFOS_HEADER_SHINY_X,    SUMMARY_INFOS_HEADER_SHINY_Y },
+            [SUMMARY_MAIN_SPRITE_HP_BAR]       = { SUMMARY_INFOS_HEADER_HP_BAR_X,   SUMMARY_INFOS_HEADER_HP_BAR_Y },
+            [SUMMARY_MAIN_SPRITE_EXP_BAR]      = { SUMMARY_INFOS_HEADER_EXP_BAR_X,  SUMMARY_INFOS_HEADER_EXP_BAR_Y },
+            [SUMMARY_MAIN_SPRITE_FRIENDSHIP_BAR] = { SUMMARY_INFOS_HEADER_FRIENDSHIP_BAR_X, SUMMARY_INFOS_HEADER_FRIENDSHIP_BAR_Y },
+        },
+        .textBoxCoords = { SUMMARY_INFOS_MISC_TEXT_BOX_X, SUMMARY_INFOS_MISC_TEXT_BOX_Y },
+        .input = Task_SummaryInput_InfosInput,
+        .handleFrontEnd = InfosPage_HandleFrontEnd,
+        .handleTextBox = InfosPage_HandleTextBox,
+        .handleHelpBar = InfosPage_HandleHelpBar,
+    },
+    [SUMMARY_PAGE_STATS] =
+    {
+        .name = COMPOUND_STRING("Stats"),
+        .windows =
+        {
+            SUMMARY_DYNAMIC_WIN_DUMMY
+        },
+        .tilemap = (const u32[])INCBIN_U32("graphics/ui_menus/mon_summary/pages/stats.bin.smolTM"),
+    },
+    [SUMMARY_PAGE_MOVES] =
+    {
+        .name = COMPOUND_STRING("Moves"),
+        .windows =
+        {
+            SUMMARY_DYNAMIC_WIN_DUMMY
+        },
+        .tilemap = (const u32[])INCBIN_U32("graphics/ui_menus/mon_summary/pages/moves.bin.smolTM"),
+    },
+};
+
+static const u32 sMonSummary_MainTiles[] = INCBIN_U32("graphics/ui_menus/mon_summary/pages/tiles.4bpp.smol");
+static const u32 sMonSummary_MainTilemap[] = INCBIN_U32("graphics/ui_menus/mon_summary/pages/blank.bin");
+static const u16 sMonSummary_MainPalette[] = INCBIN_U16("graphics/ui_menus/mon_summary/pages/tiles.gbapal");
+
+static const u8 sSummaryPrint_PageTabsBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/page_tabs.4bpp");
+static const u8 sSummaryPrint_StatusSymbolsBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/status_symbols.4bpp");
+static const u8 sSummaryPrint_MonMarkingsBlit[] = INCBIN_U8("graphics/ui_menus/mon_summary/mon_markings.4bpp");
+static const u8 sSummaryPrint_FontColors[NUM_SUMMARY_FNTCLRS][3] =
+{
+    [SUMMARY_FNTCLR_INTERFACE] = { 0, 2, 1 },
+    [SUMMARY_FNTCLR_MALE]      = { 0, 2, 15 },
+    [SUMMARY_FNTCLR_FEMALE]    = { 0, 2, 12 },
+    [SUMMARY_FNTCLR_HELP_BAR]  = { 0, 1, 0 },
+};
+
+static const u8 *const sInfosPageSummary_BerryFlavorNames[FLAVOR_COUNT] =
+{
+    [FLAVOR_SPICY]  = COMPOUND_STRING("Spicy"),
+    [FLAVOR_DRY]    = COMPOUND_STRING("Dry"),
+    [FLAVOR_SWEET]  = COMPOUND_STRING("Sweet"),
+    [FLAVOR_BITTER] = COMPOUND_STRING("Bitter"),
+    [FLAVOR_SOUR]   = COMPOUND_STRING("Sour"),
+};
