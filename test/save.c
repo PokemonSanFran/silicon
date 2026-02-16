60252445a0db98f99cdad5b1c357209e34ac26e1
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-02-16 14:10:14 -0800

    Added new mapSystem,allowing for the use of the taxi, trolley and full Arriba system https://files.catbox.moe/g3behz.mp4

--- test/save.c
+++ test/save.c
@@ -6,7 +6,7 @@
 // Please note that this simple check is not 100% foolproof, but should be able to catch most unintended shifts.
 #define T_SAVEBLOCK1_SIZE 15856
 #define T_SAVEBLOCK2_SIZE 3228
-#define T_SAVEBLOCK3_SIZE 1396
+#define T_SAVEBLOCK3_SIZE 1400
 #define T_POKEMONSTORAGE_SIZE 34144
 
 TEST("SaveBlock1 is backwards compatible")
