f2babf5c488938e3fc6a32f3ed7132ce8346b10a
mudskipper13 <MariaJumpMan001@hotmail.com>
2026-02-20 10:52:58 +0700

    fix (shopMenu): improve reloadspeed when quitting after-purchase menu

--- src/ui_shop.c
+++ src/ui_shop.c
@@ -563,15 +563,6 @@ static void Task_Shop_Idle(u8 taskId)
         case SHOP_MODE_FAILURE:
             {
                 PlaySE(SE_SELECT);
-
-                if (ShopGrid_CurrentCategoryRow() == SHOP_CATEGORY_BUY_AGAIN
-                 || ShopPurchase_IsItemOneTimePurchase(gShopMenuDataPtr->selectedItemId)
-                 || ShopPurchase_IsCategoryOneTimePurchase(ShopGrid_CurrentCategoryRow()))
-                {
-                    ShopInventory_InitCategoryLists();
-                    ShopGrid_ResetIndexes(SHOP_IDX_RESET_X_GRID | SHOP_IDX_RESET_ITEM);
-                }
-
                 gShopMenuDataPtr->itemQuantity = 0;
                 gShopMenuDataPtr->selectedItemId = ITEM_NONE;
                 ShopGrid_SwitchMode(SHOP_MODE_DEFAULT);
