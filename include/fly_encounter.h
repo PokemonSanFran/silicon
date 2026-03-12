506890b962061f98d45869d93bcf661b3e47c268
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-03-12 07:46:15 -0700

    fix (flyEncounters): prevented the ability to set terrain from overworld or trainers in sky battles [ci skip]

--- include/fly_encounter.h
+++ include/fly_encounter.h
@@ -2,8 +2,10 @@
 #define GUARD_FLYENCOUNTER_H
 
 #include "constants/fly_encounter.h"
+#include "battle_util.h"
 
 void TryFlyWildEncounter(u8 taskId, void *callback2, void *fieldCallback);
+bool8 IfSkyBattleAndOverworldTerrain(enum FieldEffectCases caseId, enum StartingStatus status);
 
 #endif // GUARD_FLYENCOUNTER_H
 
