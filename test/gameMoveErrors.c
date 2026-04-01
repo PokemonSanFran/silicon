1a680bc23ba5120b102b67b7e8ada83096eb99c8
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-04-01 11:32:56 -0700

    feat (moveErrors): updated to look for pre-evolutions [ci skip]

--- test/gameMoveErrors.c
+++ test/gameMoveErrors.c
@@ -43,7 +43,7 @@ TEST("Opposing Trainers do not have Game Move Errors")
                     }
 
                     const struct LevelUpMove *learnset = GetSpeciesLevelUpLearnset(species);
-                    for (u32 j = 0; learnset[j].move != LEVEL_UP_MOVE_END;j++)
+                    for (u32 j = 0; learnset[j].move != LEVEL_UP_MOVE_END; j++)
                     {
                         if (learnset[j].move == move)
                         {
@@ -56,7 +56,7 @@ TEST("Opposing Trainers do not have Game Move Errors")
                         continue;
 
                     const u16 *teachables = GetSpeciesTeachableLearnset(species);
-                    for (u32 j = 0; teachables[j] != MOVE_UNAVAILABLE;j++)
+                    for (u32 j = 0; teachables[j] != MOVE_UNAVAILABLE; j++)
                     {
                         if (teachables[j] == move)
                         {
@@ -69,7 +69,7 @@ TEST("Opposing Trainers do not have Game Move Errors")
                         continue;
 
                     const u16 *eggMoves = GetSpeciesEggMoves(species);
-                    for (u32 j = 0; eggMoves[j] != MOVE_UNAVAILABLE;j++)
+                    for (u32 j = 0; eggMoves[j] != MOVE_UNAVAILABLE; j++)
                     {
                         if (eggMoves[j] == move)
                         {
@@ -81,11 +81,89 @@ TEST("Opposing Trainers do not have Game Move Errors")
                     if (retVal)
                         continue;
 
+                    u16 currentSpecies = species;
+                    for (u32 evoDepth = 0; evoDepth < 5 && !retVal; evoDepth++)
+                    {
+                        bool32 foundPrevo = FALSE;
+                        u16 prevoSpecies = SPECIES_NONE;
+
+                        for (u32 s = 1; s < NUM_SPECIES; s++)
+                        {
+                            if (!IsSpeciesEnabled(s))
+                                continue;
+
+                            const struct Evolution *evolutions = GetSpeciesEvolutions(s);
+                            if (evolutions == NULL)
+                                continue;
+
+                            for (u32 e = 0; evolutions[e].method != EVOLUTIONS_END; e++)
+                            {
+                                if (SanitizeSpeciesId(evolutions[e].targetSpecies) == currentSpecies)
+                                {
+                                    prevoSpecies = s;
+                                    foundPrevo = TRUE;
+
+                                    const struct LevelUpMove *prevLearnset = GetSpeciesLevelUpLearnset(prevoSpecies);
+                                    for (u32 j = 0; prevLearnset[j].move != LEVEL_UP_MOVE_END; j++)
+                                    {
+                                        if (prevLearnset[j].move == move)
+                                        {
+                                            retVal = TRUE;
+                                            break;
+                                        }
+                                    }
+
+                                    if (!retVal)
+                                    {
+                                        const u16 *prevTeachables = GetSpeciesTeachableLearnset(prevoSpecies);
+                                        for (u32 j = 0; prevTeachables[j] != MOVE_UNAVAILABLE; j++)
+                                        {
+                                            if (prevTeachables[j] == move)
+                                            {
+                                                retVal = TRUE;
+                                                break;
+                                            }
+                                        }
+                                    }
+
+                                    if (!retVal)
+                                    {
+                                        const u16 *prevEggMoves = GetSpeciesEggMoves(prevoSpecies);
+                                        for (u32 j = 0; prevEggMoves[j] != MOVE_UNAVAILABLE; j++)
+                                        {
+                                            if (prevEggMoves[j] == move)
+                                            {
+                                                retVal = TRUE;
+                                                break;
+                                            }
+                                        }
+                                    }
+
+                                    break;
+                                }
+                            }
+
+                            if (foundPrevo)
+                                break;
+                        }
+
+                        if (retVal)
+                            break;
+
+                        if (!foundPrevo)
+                            break;
+
+                        currentSpecies = prevoSpecies;
+                    }
+
+                    if (retVal)
+                        continue;
+
                     Test_ExitWithResult(TEST_RESULT_FAIL, __LINE__, ":L%s:%d: Pokemon %d for %S is a %S with %S.", gTestRunnerState.test->filename, __LINE__, monIndex, GetTrainerNameFromId(trainerId), GetSpeciesName(species), GetMoveName(move));
                 }
             }
-
         }
     }
 }
+
 
