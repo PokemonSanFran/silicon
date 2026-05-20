672c477d81ff3518141d509c9428dbf5adcbbcd7
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-05-20 06:43:28 -0700

    fix: removed problematic inc include

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1814,7 +1814,6 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 .include "data/scripts/nickname.inc" @ Battle Settings: Nickname
 .include "data/scripts/cutscene.inc"
 .include "data/scripts/fishing.inc"
-.include "data/scripts/surprise_trade.inc" @ surpriseTrade
 .include "data/scripts/save.inc" @ autoSave
 .include "data/scripts/phenomenon.inc" @ autoSave
 .include "data/scripts/pokemart.inc" @ shopMenu
