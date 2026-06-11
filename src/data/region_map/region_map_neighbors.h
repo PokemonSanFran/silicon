5bff0bcb2aa0c025809a0d461d604a3cdfef1f97
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-06-11 06:58:45 -0700

    feat (visitedFlags): added neighbors file [ci skip]

---
+++ src/data/region_map/region_map_neighbors.h
@@ -0,0 +1,423 @@
+static struct RegionMapNeighbors regionMapNeighbors[] =
+{
+    {
+        .mapA = MAPSEC_CURENO_PORT,
+        .mapB = MAPSEC_PERLACIA_CITY,
+        .destination = MAPSEC_ROUTE_2,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_QIU_VILLAGE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_HALERBA_CITY,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_PETAROSA_BOROUGH,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CRESALTA_VISTA,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_IRISINA_TOWN,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_MERMEREZA_CITY,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CURENO_PORT,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CUCONU_TOWN,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_OROLAND,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_ESPULEE_OUTSKIRTS,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CHASILLA,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CAPHE_CITY,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_HODOU_CITY,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_TORA_TOWN,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_PERLACIA_CITY,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_GLAVEZ_HILL,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_HALAI_ISLAND,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_TIRABUDIN_PLACE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_NAVAL_BASE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_PINTILLONHOUSE,
+    },
+    {
+        .mapA = MAPSEC_POPIDORA_PIER,
+        .mapB = MAPSEC_ROUTE12,
+        .destination = MAPSEC_ANBEH_BEND,
+    },
+    {
+        .mapA = MAPSEC_POPIDORA_PIER,
+        .mapB = MAPSEC_ROUTE11,
+        .destination = MAPSEC_ANBEH_BEND,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_12,
+        .mapB = MAPSEC_ROUTE_11,
+        .destination = MAPSEC_ANBEH_BEND,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_FORT_YOBU,
+    },
+    {
+        .mapA = MAPSEC_POPIDORA_PIER,
+        .mapB = MAPSEC_ROUTE4,
+        .destination = MAPSEC_FERRYBUILDING,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_POPIDORA_PIER,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_SHARPRISESTADIUM,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_OROLAND_COLISEUM,
+    },
+    {
+        .mapA = MAPSEC_ESPULEE_OUTSKIRTS,
+        .mapB = MAPSEC_ROBINWILLTUNNEL,
+        .destination = MAPSEC_PIOCA_BRIDGE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_LEAVERRA_FOREST,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_WISHAAST_LAKE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_WAJABI_LAKE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_ZENZU_ISLAND,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_ARANTRAZ,
+    },
+    {
+        .mapA = MAPSEC_ANBEH_BEND,
+        .mapB = MAPSEC_ROUTE16,
+        .destination = MAPSEC_ROUTE11,
+    },
+    {
+        .mapA = MAPSEC_ANBEH_BEND,
+        .mapB = MAPSEC_PETAROSA_BOROUGH,
+        .destination = MAPSEC_ROUTE11,
+    },
+    {
+        .mapA = MAPSEC_PETAROSA_BOROUGH,
+        .mapB = MAPSEC_ROUTE16,
+        .destination = MAPSEC_ROUTE11,
+    },
+    {
+        .mapA = MAPSEC_ROUTE11,
+        .mapB = MAPSEC_PINTILLONHOUSE,
+        .destination = MAPSEC_ROUTE16,
+    },
+    {
+        .mapA = MAPSEC_HALERBAWILDS,
+        .mapB = MAPSEC_CRESALTA_VISTA,
+        .destination = MAPSEC_ROUTE22,
+    },
+    {
+        .mapA = MAPSEC_HALERBAWILDS,
+        .mapB = MAPSEC_QIU_VILLAGE,
+        .destination = MAPSEC_ROUTE18,
+    },
+    {
+        .mapA = MAPSEC_PERLACIA_CITY,
+        .mapB = MAPSEC_POPIDORA_PIER,
+        .destination = MAPSEC_ROUTE4,
+    },
+    {
+        .mapA = MAPSEC_PERLACIA_CITY,
+        .mapB = MAPSEC_HODOU_CITY,
+        .destination = MAPSEC_ROUTE4,
+    },
+    {
+        .mapA = MAPSEC_POPIDORA_PIER,
+        .mapB = MAPSEC_HODOU_CITY,
+        .destination = MAPSEC_ROUTE4,
+    },
+    {
+        .mapA = MAPSEC_CAPHE_CITY,
+        .mapB = MAPSEC_MERMEREZA_CITY,
+        .destination = MAPSEC_ROUTE10,
+    },
+    {
+        .mapA = MAPSEC_GLAVEZ_HILL,
+        .mapB = MAPSEC_MERMEREZA_CITY,
+        .destination = MAPSEC_ROUTE8,
+    },
+    {
+        .mapA = MAPSEC_PINTILLONHOUSE,
+        .mapB = MAPSEC_IRISINA_TOWN,
+        .destination = MAPSEC_ROUTE14,
+    },
+    {
+        .mapA = MAPSEC_MERMEREZA_CITY,
+        .mapB = MAPSEC_IRISINA_TOWN,
+        .destination = MAPSEC_ROUTE5,
+    },
+    {
+        .mapA = MAPSEC_POPIDORA_PIER,
+        .mapB = MAPSEC_ARANTRAZ,
+        .destination = MAPSEC_ROUTE_D,
+    },
+    {
+        .mapA = MAPSEC_CUCONU_TOWN,
+        .mapB = MAPSEC_GLAVEZ_HILL,
+        .destination = MAPSEC_ROUTE6,
+    },
+    {
+        .mapA = MAPSEC_IRISINA_TOWN,
+        .mapB = MAPSEC_TORGEOT_CLIMB,
+        .destination = MAPSEC_ROUTE3,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_A,
+        .mapB = MAPSEC_ROUTE_E,
+        .destination = MAPSEC_ROUTE_C,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_A,
+        .mapB = MAPSEC_HALAI_ISLAND,
+        .destination = MAPSEC_ROUTE_C,
+    },
+    {
+        .mapA = MAPSEC_HALAI_ISLAND,
+        .mapB = MAPSEC_ROUTE_E,
+        .destination = MAPSEC_ROUTE_C,
+    },
+    {
+        .mapA = MAPSEC_CURENO_PORT,
+        .mapB = MAPSEC_GLAVEZ_HILL,
+        .destination = MAPSEC_ROUTE_1,
+    },
+    {
+        .mapA = MAPSEC_CURENO_PORT,
+        .mapB = MAPSEC_SHARPRISESTADIUM,
+        .destination = MAPSEC_ROUTE2,
+    },
+    {
+        .mapA = MAPSEC_CURENO_PORT,
+        .mapB = MAPSEC_PERLACIA_CITY,
+        .destination = MAPSEC_ROUTE2,
+    },
+    {
+        .mapA = MAPSEC_PERLACIA_CITY,
+        .mapB = MAPSEC_SHARPRISESTADIUM,
+        .destination = MAPSEC_ROUTE2,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_SECRET_PATH,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_4,
+        .mapB = MAPSEC_OROLAND,
+        .destination = MAPSEC_NONGYU_BRIDGE,
+    },
+    {
+        .mapA = MAPSEC_ZENZU_ISLAND,
+        .mapB = MAPSEC_OROLAND,
+        .destination = MAPSEC_NONGYU_BRIDGE,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_4,
+        .mapB = MAPSEC_ZENZU_ISLAND,
+        .destination = MAPSEC_NONGYU_BRIDGE,
+    },
+    {
+        .mapA = MAPSEC_QIU_VILLAGE,
+        .mapB = MAPSEC_WAJABI_LAKE,
+        .destination = MAPSEC_ROUTE20,
+    },
+    {
+        .mapA = MAPSEC_TORA_TOWN,
+        .mapB = MAPSEC_CAPHE_CITY,
+        .destination = MAPSEC_ROUTE9,
+    },
+    {
+        .mapA = MAPSEC_PINTILLONHOUSE,
+        .mapB = MAPSEC_TIRABUDIN_PLACE,
+        .destination = MAPSEC_ROUTE7,
+    },
+    {
+        .mapA = MAPSEC_PETAROSA_BOROUGH,
+        .mapB = MAPSEC_CRESALTA_VISTA,
+        .destination = MAPSEC_ROUTE13, 
+    },
+    {
+        .mapA = MAPSEC_HALAI_ISLAND,
+        .mapB = MAPSEC_FERRYBUILDING,
+        .destination = MAPSEC_ROUTE_A,
+    },
+    {
+        .mapA = MAPSEC_HALAI_ISLAND,
+        .mapB = MAPSEC_ROUTE_C,
+        .destination = MAPSEC_ROUTE_A,
+    },
+    {
+        .mapA = MAPSEC_ROUTE_C,
+        .mapB = MAPSEC_FERRYBUILDING,
+        .destination = MAPSEC_ROUTE_A,
+    },
+    {
+        .mapA = MAPSEC_LEAVERRA_FOREST,
+        .mapB = MAPSEC_ESPULEE_OUTSKIRTS,
+        .destination = MAPSEC_ROUTE100,
+    },
+    {
+        .mapA = MAPSEC_OROLAND,
+        .mapB = MAPSEC_WISHAAST_LAKE,
+        .destination = MAPSEC_ROUTE98,
+    },
+    {
+        .mapA = MAPSEC_OROLAND,
+        .mapB = MAPSEC_OROLAND_COLISEUM,
+        .destination = MAPSEC_ROUTE98,
+    },
+    {
+        .mapA = MAPSEC_WISHAAST_LAKE,
+        .mapB = MAPSEC_OROLAND_COLISEUM,
+        .destination = MAPSEC_ROUTE98,
+    },
+    {
+        .mapA = MAPSEC_CHASILLA,
+        .mapB = MAPSEC_OROLAND,
+        .destination = MAPSEC_ROUTE99,
+    },
+    {
+        .mapA = MAPSEC_ANBEH_BEND,
+        .mapB = MAPSEC_CAPHE_CITY,
+        .destination = MAPSEC_ROUTE12,
+    },
+    {
+        .mapA = MAPSEC_HALAI_ISLAND,
+        .mapB = MAPSEC_OROLAND,
+        .destination = MAPSEC_ROUTE_B,
+    },
+    {
+        .mapA = MAPSEC_ESPULEE_OUTSKIRTS,
+        .mapB = MAPSEC_CHASILLA,
+        .destination = MAPSEC_ROUTE_E,
+    },
+    {
+        .mapA = MAPSEC_ARANTRAZ,
+        .mapB = MAPSEC_CHASILLA,
+        .destination = MAPSEC_ROUTE_E,
+    },
+    {
+        .mapA = MAPSEC_ARANTRAZ,
+        .mapB = MAPSEC_ESPULEE_OUTSKIRTS,
+        .destination = MAPSEC_ROUTE_E,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_HALERBAWILDS,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_SHARPRISESPIRE,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_PARC,
+    },
+    {
+        .mapA = 0,
+        .mapB = 0,
+        .destination = MAPSEC_CHAPEL_OF_CHIMES,
+    },
+    {
+        .mapA = MAPSEC_FORT_YOBU,
+        .mapB = MAPSEC_PINTILLONHOUSE,
+        .destination = MAPSEC_ROUTE16,
+    },
+};
