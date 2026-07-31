2afbba8bb1960aff96462450162f203a3825e751
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-07-30 19:58:00 -0700

    feat (siliconFrontier): Added first version of frontier scripts [ci skip]

--- include/silicon_battle_frontier.h
+++ include/silicon_battle_frontier.h
@@ -2,6 +2,7 @@
 #define GUARD_SILICON_BATTLE_FRONTIER_H
 
 #include "constants/silicon_battle_frontier.h"
+#include "constants/trainers.h"
 
 struct SiliconFrontierStreaks
 {
@@ -9,6 +10,27 @@ struct SiliconFrontierStreaks
     u16 longestStreak;
     enum SiliconFrontierPartner longestStreakPartner;
     //struct Pokemon longestSteakMons[MAX_FRONTIER_PARTY_SIZE];
+};
+
+struct SiliconFrontierTrainers
+{
+    u16 unlockFlag;
+    u8 trainerClass;
+    const u8 *trainerName;
+    const u8 *introText;
+    const u8 *playerWinText;
+    const u8 *playerLossText;
+    const u16 monSet[SILICON_FRONTIER_MON_SET_SIZE];
+};
+
+struct SiliconFrontierData
+{
+    u16 silverBadge;
+    enum SiliconFrontierTrainerIds silverBoss;
+    u16 goldBadge;
+    enum SiliconFrontierTrainerIds goldBoss;
+    const u8 *name;
+    mapsec_s16_t mapsec;
 };
 
 #endif // GUARD_SILICON_BATTLE_FRONTIER_H
