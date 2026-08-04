54481af5cd3f46e67ba91f0198c22b0f96c45d79
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-08-04 16:00:21 -0700

    feat (siliconFrontier): added milestone and badge names to frontier data struct [ci skip]

--- src/data/silicon_frontier/facilities.h
+++ src/data/silicon_frontier/facilities.h
@@ -8,10 +8,20 @@ const struct SiliconFrontierData gSiliconFrontierData[SILICON_FACILITY_COUNT] =
             SILICON_FRONTIER_TRAINER_TOWER_GOLD,
             SILICON_FRONTIER_TRAINER_TOWER,
         },
+        .milestone =
+        {
+            SILICON_FRONTIER_STREAK_LENGTH_SILVER,
+            SILICON_FRONTIER_STREAK_LENGTH_GOLD,
+        },
         .badge =
         {
             FLAG_SYS_RESTORED_TOWER_SILVER,
             FLAG_SYS_RESTORED_TOWER_GOLD,
+        },
+        .badgeName =
+        {
+            COMPOUND_STRING("Silver Tower Badge"),
+            COMPOUND_STRING("Gold Tower Badge"),
         },
         .originalName = COMPOUND_STRING("Battle Tower"),
         .name = COMPOUND_STRING("Battle Jam"),
@@ -25,10 +35,20 @@ const struct SiliconFrontierData gSiliconFrontierData[SILICON_FACILITY_COUNT] =
             SILICON_FRONTIER_TRAINER_DOYLE_GOLD,
             SILICON_FRONTIER_TRAINER_DOYLE,
         },
+        .milestone =
+        {
+            SILICON_FRONTIER_STREAK_LENGTH_SILVER,
+            SILICON_FRONTIER_STREAK_LENGTH_GOLD,
+        },
         .badge =
         {
             FLAG_SYS_RESTORED_ARCADE_SILVER,
             FLAG_SYS_RESTORED_ARCADE_GOLD,
+        },
+        .badgeName =
+        {
+            COMPOUND_STRING("Silver Arcade Badge"),
+            COMPOUND_STRING("Gold Arcade Badge"),
         },
         .originalName = COMPOUND_STRING("Battle Arcade"),
         .name = COMPOUND_STRING("Battle Studio"),
@@ -42,10 +62,20 @@ const struct SiliconFrontierData gSiliconFrontierData[SILICON_FACILITY_COUNT] =
             SILICON_FRONTIER_TRAINER_SPARRING_GOLD,
             SILICON_FRONTIER_TRAINER_SPARRING,
         },
+        .milestone =
+        {
+            SILICON_FRONTIER_STREAK_LENGTH_SILVER,
+            SILICON_FRONTIER_STREAK_LENGTH_GOLD,
+        },
         .badge =
         {
             FLAG_SYS_RESTORED_DOJO_SILVER,
             FLAG_SYS_RESTORED_DOJO_GOLD,
+        },
+        .badgeName =
+        {
+            COMPOUND_STRING("Silver Dojo Badge"),
+            COMPOUND_STRING("Gold Dojo Badge"),
         },
         .originalName = COMPOUND_STRING("Restricted Sparring"),
         .name = COMPOUND_STRING("Restricted Sparring"),
@@ -59,10 +89,20 @@ const struct SiliconFrontierData gSiliconFrontierData[SILICON_FACILITY_COUNT] =
             SILICON_FRONTIER_TRAINER_IMELDA_GOLD,
             SILICON_FRONTIER_TRAINER_IMELDA,
         },
+        .milestone =
+        {
+            SILICON_FRONTIER_STREAK_LENGTH_SILVER,
+            SILICON_FRONTIER_STREAK_LENGTH_GOLD,
+        },
         .badge =
         {
-            FLAG_SYS_RESTORED_ARCADE_SILVER,
-            FLAG_SYS_RESTORED_ARCADE_GOLD,
+            FLAG_SYS_RESTORED_FACTORY_SILVER,
+            FLAG_SYS_RESTORED_FACTORY_GOLD,
+        },
+        .badgeName =
+        {
+            COMPOUND_STRING("Silver Factory Badge"),
+            COMPOUND_STRING("Gold Factory Badge"),
         },
         .originalName = COMPOUND_STRING("Battle Factory"),
         .name = COMPOUND_STRING("Battle Convention"),
