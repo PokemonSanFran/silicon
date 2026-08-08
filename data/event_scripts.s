91aff1ba76ca3daf59027199e8202937b74c628c
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-08-08 11:10:21 -0700

    refactor (siliconFrontier): started moving battle jam specific text into individual text files [ci skip]

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1813,6 +1813,9 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 .include "data/scripts/cutscene.inc"
 .include "data/scripts/fishing.inc"
 .include "data/scripts/silicon_frontier/general.inc" @ siliconFrontier
+.include "data/scripts/silicon_frontier/text_battle_jam.inc" @ siliconFrontier
+.include "data/scripts/silicon_frontier/text_restricted_sparring.inc" @ siliconFrontier
+.include "data/scripts/silicon_frontier/text_general.inc" @ siliconFrontier
 .include "data/scripts/save.inc" @ autoSave
 .include "data/scripts/phenomenon.inc" @ autoSave
 .include "data/scripts/pokemart.inc" @ shopMenu
